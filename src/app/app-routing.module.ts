c89ab31aa47eed6227ed9f82b90c2d67f559bd7c
Sindhu Gowda <SindhuGowda.amazeInc@gmail.com>
2022-04-08 12:18:40 +0530

    dd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AgentManagerDashboardComponent } from './agent-manager-dashboard/agent-manager-dashboard.component';
+import { CompanySetupComponent } from './company-setup/company-setup.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { LoginComponent } from './login/login.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { StaffSetupComponent } from './staff-setup/staff-setup.component';
 
 const routes: Routes = [
 
@@ -12,6 +15,9 @@ const routes: Routes = [
   {path:'Header',component:HeaderComponent},
   {path:'Sidebar',component:SidebarComponent},
   {path:'Footer',component:FooterComponent},
+  {path:'CompanySetup',component:CompanySetupComponent},
+  {path:'StaffSetup',component:StaffSetupComponent},
+  {path:'AgentManagerDashboard',component:AgentManagerDashboardComponent},
 ];
 
 @NgModule({
